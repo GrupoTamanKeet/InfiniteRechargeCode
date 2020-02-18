15b1562a5a658ceb12deeca3fc3c5bcbe3d3f638
JorgePerC <42452493+JorgePerC@users.noreply.github.com>
2020-02-17 22:37:26 -0600

    same

--- CODIGO OFICIAL ROBOT/src/main/java/frc/robot/subsystems/MoveDisk.java
+++ CODIGO OFICIAL ROBOT/src/main/java/frc/robot/subsystems/MoveDisk.java
@@ -6,10 +6,8 @@ import frc.robot.hardware.Constantes;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 
@@ -17,38 +15,35 @@ public class MoveDisk{
 
     public static DoubleSolenoid pistondisco;
     public static WPI_TalonSRX motordisco;
-    private I2C.Port i2cPort = I2C.Port.kOnboard;
     private ColorSensor colorSensor;
     
     int ForwardChannel = 3;
-
 
     public MoveDisk (){
         pistondisco = new DoubleSolenoid(Constantes.ConexionPistonDisco,ForwardChannel);
         motordisco = new WPI_TalonSRX(Constantes.ConexionMotorDisco);
         colorSensor = new ColorSensor();
     }
-    public void MoverDisco(){
+    public void moverDisco(){
         motordisco.set(ControlMode.PercentOutput, .5);
     }
 
-    public void AbrirPiston() {
+    public void abrirPiston() {
         pistondisco.set(Value.kForward);
     }
 
-    public void CerrarPiston(){
+    public void cerrarPiston(){
         pistondisco.set(Value.kReverse);
 
     }
     public void funcionar(){
         if (Robot.control.readJoystickButtons(Constantes.LG_B3)== true){
-            MoverDisco();
-        
+            moverDisco();
         }
         colorSensor.leerColor();
     }
     
-    public void PararMotor(){
+    public void pararMotor(){
         motordisco.stopMotor();
         motordisco.setVoltage(0);
     }
